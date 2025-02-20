95109652e0bd058aa781a0cd3516962b2eb12724
carlajl14 <113455597+carlajl14@users.noreply.github.com>
2025-02-20 07:28:15 +0000

    changes

--- app.py
+++ app.py
@@ -1,47 +1,37 @@
 from flask import Flask, request, jsonify
 import tensorflow as tf
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input, decode_predictions
+from tensorflow.keras.applications import MobileNetV2, preprocess_input, decode_predictions
 from PIL import Image
 import numpy as np
 import io
 
 app = Flask(__name__)
 
-# Cargar el modelo pre-entrenado MobileNetV2
-#def load_model():
-    #return MobileNetV2(weights='imagenet')
+def load_model():
+    return MobileNetV2(weights='imagenet')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Verificar el encabezado de RapidAPI
-    #rapidapi_secret = request.headers.get('X-RapidAPI-Proxy-Secret')
-    #if rapidapi_secret != '5ec60b00-eddc-11ef-9c15-59f608f93374':
-        #return jsonify({'error': 'Acceso no autorizado'}), 403
-
-    # Obtener la imagen de la solicitud
     file = request.files['image']
-    img = Image.open(io.BytesIO(file.read()))
+    if not file:
+        return jsonify({'error': 'No file uploaded'}), 400
 
-    # Preprocesar la imagen
-    img = img.resize((224, 224))  # Cambiar el tamaño para MobileNetV2
+    img = Image.open(io.BytesIO(file.read()))
+    img = img.resize((224, 224))
     img_array = np.array(img)
     img_array = np.expand_dims(img_array, axis=0)
     img_array = preprocess_input(img_array)
 
-    # Cargar el modelo y realizar la predicción
     model = load_model()
     predictions = model.predict(img_array)
-    decoded_predictions = decode_predictions(predictions, top=1)[0]
+    decoded_predictions = decode_predictions(predictions, top=3)[0]
 
-    # Formatear la respuesta
-    top_prediction = decoded_predictions[0]
-    result = {
-        "label": top_prediction[0],
-        "description": top_prediction[1],
-        "score": float(top_prediction[2])
-    }
+    results = [
+        {"label": pred[1], "confidence": float(pred[2])}
+        for pred in decoded_predictions
+    ]
 
-    return jsonify(result)
+    return jsonify(results)
 
 if __name__ == '__main__':
     app.run(debug=False)
