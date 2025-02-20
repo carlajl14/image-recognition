d2baab719a80e895777d5dc504fa326b9585dfbe
carlajl14 <113455597+carlajl14@users.noreply.github.com>
2025-02-20 08:25:34 +0000

    import distutils

--- predict.py
+++ predict.py
@@ -4,6 +4,7 @@ import numpy as np
 import io
 from model import load_model
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input, decode_predictions
+import distutils
 
 model = load_model()
 
